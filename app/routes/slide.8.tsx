0232d1b41c18ba7e4bdb0e8993679a4cd538366d
andrewgremlich <andrew.gremlich@gmail.com>
2025-01-02 17:56:49 -0700

    swapping slides

--- app/routes/slide.8.tsx
+++ app/routes/slide.8.tsx
@@ -1,7 +1,8 @@
 import { useLoaderData } from "@remix-run/react";
-import { Image } from "summit-kit";
+import { Link, P, Ul } from "summit-kit";
 
 import { Layout } from "@/Layout";
+import { Item } from "@/Body";
 import { slideLoader } from "utils/slideLoader";
 import type { SlideLoaderData } from "utils/types";
 
@@ -11,12 +12,37 @@ export default function Index() {
 	const { number } = useLoaderData<SlideLoaderData>();
 
 	return (
-		<Layout currentSlide={number}>
-			<Image
-				src="https://fourminutebooks.com/wp-content/uploads/2023/02/marcus-aurelius-quotes-27.jpg"
-				alt="Try hard by Marcus Aurelius"
-				width="100%"
-			/>
+		<Layout slideTitle="Firebase Integration" currentSlide={number}>
+			<P>sorry not partykit...</P>
+			<Ul>
+				<Item>
+					Firebase integration with{" "}
+					<Link
+						href="https://github.com/andrewgremlich/remix-and-three/blob/main/app/utils/firebase.ts"
+						target="_blank"
+					>
+						firebase.ts
+					</Link>
+				</Item>
+				<Item>
+					Playground implementation with{" "}
+					<Link
+						href="https://github.com/andrewgremlich/remix-and-three/blob/main/app/routes/play.tsx"
+						target="_blank"
+					>
+						routes/play.tsx
+					</Link>
+				</Item>
+				<Item>
+					Controller implementation with{" "}
+					<Link
+						href="https://github.com/andrewgremlich/remix-and-three/blob/main/app/routes/controller.tsx"
+						target="_blank"
+					>
+						routes/controller.tsx
+					</Link>
+				</Item>
+			</Ul>
 		</Layout>
 	);
 }
